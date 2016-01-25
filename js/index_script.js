6aa1d4063ab5fa9fc397fab72ab499a2e606720d
Mikhail Logachev <miha.loga@gmail.com>
2016-01-25 22:21:55 +0300

    Version 2

--- js/index_script.js
+++ js/index_script.js
@@ -1,16 +1,31 @@
-$(document).ready(function(){       
-            var scroll_pos = 0;
-            $(document).scroll(function() { 
-                scroll_pos = $(this).scrollTop();
-                console.log(scroll_pos);
-                if(scroll_pos > 150) {
-                    $("#index-header").stop().animate({
-                        backgroundColor: "rgba(0,0,0,1)"
-                    }, 50);
-                } else {
-                    $("#index-header").stop().animate({
-                        backgroundColor: "rgba(0,0,0,0)"
-                    }, 50); 
-                }
-            });
-        });
+$(document).ready(function(){  
+
+    var scroll_pos = 0;
+    //header bar color change 
+    $(document).scroll(function() { 
+        
+        scroll_pos = $(this).scrollTop();
+        console.log(scroll_pos);
+        
+        if(scroll_pos > 150) {
+            $("#index-header").stop().animate({
+                backgroundColor: "rgba(240,240,240,1)"
+            }, 50);
+        } else {
+            $("#index-header").stop().animate({
+                backgroundColor: "rgba(240,240,240,0)"
+            }, 50); 
+        }
+    });
+
+    $('.page-nav').on('click', function(event) {
+        var target = $($(this).attr('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+
+    });
+});
